3a65ed338e9c58e64e14b39fe39327df42ccffc7
mwcz <mwc@clayto.com>
2016-10-11 13:58:32 -0400

    remove unused workaround

--- src/states/boot.ts
+++ src/states/boot.ts
@@ -1,8 +1,6 @@
 import * as Phaser from 'phaser';
 import * as Tiled from 'phaser-tiled';
 
-// declare let Tiled: any; // hack to make 'Tiled' available; the tiled plugin is weird around AMD
-
 export default class BootState extends Phaser.State {
 
     preload() {
