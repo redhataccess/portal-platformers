23286dbccbd18e57596a271a155050d6506adedb
Jared Sprague <jared@caramelcode.com>
2017-07-14 15:22:22 -0400

    changed defaulit port to 8080

--- server/AppServer.js
+++ server/AppServer.js
@@ -1,20 +1,20 @@
-var NODEJS = typeof module !== 'undefined' && module.exports;
+const NODEJS = typeof module !== 'undefined' && module.exports;
 
 /**
  * This module contains all of the app logic and state,
  * @param io
  * @constructor
  */
-var AppServer = function (io) {
+let AppServer = function (io) {
     //  Scope.
-    var self = this;
+    let self = this;
 
     self.io = io;
 
     // Example state
-    var updateCount = 0;
+    let updateCount = 0;
 
-    setInterval(function() {
+    setInterval(function () {
         // send to all clients
         self.io.emit('server_message', ++updateCount);
     }, 100);
@@ -23,15 +23,15 @@ var AppServer = function (io) {
 
         console.log('Client connected headers:', JSON.stringify(socket.handshake));
 
-        var name = socket.handshake.query.name;
+        let name = socket.handshake.query.name;
 
         console.log("Name:", name);
 
         self.io.emit('client_joined', "Client joined: " + name);
 
         socket.on('binary_message', function (msg) {
-            var ab = toArrayBuffer(msg);
-            var arr = new Int32Array(ab);
+            let ab = toArrayBuffer(msg);
+            let arr = new Int32Array(ab);
             console.log(arr[0]);
         });
 
@@ -45,9 +45,9 @@ var AppServer = function (io) {
         });
 
         function toArrayBuffer(buffer) {
-            var ab = new ArrayBuffer(buffer.length);
-            var view = new Uint8Array(ab);
-            for (var i = 0; i < buffer.length; ++i) {
+            let ab = new ArrayBuffer(buffer.length);
+            let view = new Uint8Array(ab);
+            for (let i = 0; i < buffer.length; ++i) {
                 view[i] = buffer[i];
             }
             return ab;
