b55c4663aafdfa63db9977ce58ce27f35018a9d8
Krystal Moore <kmoore@redhat.com>
2017-07-14 13:28:07 -0400

    Change splash screen duration

--- client/config.js
+++ client/config.js
@@ -1,9 +1,6 @@
 Game.config = {
     splash: {
         fontSize: 44,
-        duration: 1000,
-    },
-    // fontSize: 20,
-    fontFamily: 'comic',
-    // gameTitle: 'Portal Platformers',
+        duration: 4000,
+    }
 };
